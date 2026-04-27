7b259cf9fdcfa27afe4dfa0afe3cef93d73082ef
junk187block <junk.187block@gmail.com>
2026-04-28 02:07:16 +0400

    Cleanup repository: remove non-website files

--- scratch/inspect_genai.js
+++
@@ -1,5 +0,0 @@
-import { GoogleGenAI } from '@google/genai';
-const ai = new GoogleGenAI({ apiKey: 'test' });
-console.log('Keys of GoogleGenAI instance:', Object.keys(ai));
-if (ai.models) console.log('Keys of ai.models:', Object.keys(ai.models));
-console.log('Is getGenerativeModel a function?', typeof ai.getGenerativeModel === 'function');
